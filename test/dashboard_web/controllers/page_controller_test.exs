ef01c79f3c776a79bfba3b2a6be2c75b79cfecf8
Adrien Gregoire <adrien.gregoire@epitech.eu>
2026-09-23 20:27:23 +0200

    fix(tests): edit welcome msg

--- test/dashboard_web/controllers/page_controller_test.exs
+++ test/dashboard_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule DashboardWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Welcome to Dashboard"
   end
 end
