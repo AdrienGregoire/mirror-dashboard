ed32045ef4e7f4f418e0f14bad7ad77f354f12c0
Marc-Antoine <marcantoinesadio@gmail.com>
2026-09-24 18:41:26 +0200

    fix(tests): update home page test to the new landing page

--- test/dashboard_web/controllers/page_controller_test.exs
+++ test/dashboard_web/controllers/page_controller_test.exs
@@ -1,8 +1,19 @@
+#
+## EPITECH PROJECT, 2026
+## page_controller_test.exs
+## File description:
+## ExUnit for page_controller
+#
+
 defmodule DashboardWeb.PageControllerTest do
   use DashboardWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
+  test "GET / shows the home page with login and register links", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    html = html_response(conn, 200)
+
+    assert html =~ "Welcome to Dashboard"
+    assert html =~ ~s(href="/login")
+    assert html =~ ~s(href="/register")
   end
 end
